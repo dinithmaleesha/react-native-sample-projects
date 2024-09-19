868b72a783f3af5d2cb61010cec1a8ec607bb457
Dinith Maleesha <maleesha.live123@gmail.com>
2024-09-19 23:56:59 +0530

    handleLogin

--- AwesomeProject/src/screens/HomeScreen.tsx
+++ AwesomeProject/src/screens/HomeScreen.tsx
@@ -1,25 +1,45 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { RootState } from '../store/store'
+import { View, Text, StyleSheet, Button } from 'react-native';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../store/store';
+import { loginAction } from '../store/userActions';
 
 const HomeScreen = () => {
-  const isLogedIn = useSelector((state: RootState) => state.data.isLoggedIn)
-  console.log(isLogedIn);
-  
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state: RootState) => state.data.isLoggedIn);
+
+  console.log(isLoggedIn);
+
+  const handleLogin = () => {
+    dispatch(loginAction());
+    console.log('Logged In..!');
+  };
+
   return (
     <View style={styles.content}>
-      <Text>HomeScreen</Text>
+      {isLoggedIn ? (
+        <Text style={styles.text}>Hello World..!</Text>
+      ) : (
+        <View>
+          <Text style={[styles.text, { color: 'red' }]}>You are not Logged In</Text>
+          <Button title='Log in' onPress={handleLogin} />
+        </View>
+      )}
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
 const styles = StyleSheet.create({
   content: {
     justifyContent: 'center',
     alignItems: 'center',
-    flex: 1
-  }
-})
+    flex: 1,
+  },
+  text: {
+    fontSize: 20,
+    color: 'black',
+    marginBottom: 10,
+  },
+});
