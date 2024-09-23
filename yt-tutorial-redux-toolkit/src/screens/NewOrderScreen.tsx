b8f282b7357f71c61af5f9cf6ed5d2713a116154
Dinith Maleesha <maleesha.live123@gmail.com>
2024-09-23 13:57:15 +0530

    changes

--- yt-tutorial-redux-toolkit/src/screens/NewOrderScreen.tsx
+++ yt-tutorial-redux-toolkit/src/screens/NewOrderScreen.tsx
@@ -1,5 +1,5 @@
-import React, {useCallback} from 'react';
-import {View, StyleSheet, SafeAreaView, Alert, } from 'react-native';
+import React, { useCallback } from 'react';
+import { View, StyleSheet, SafeAreaView, Alert, } from 'react-native';
 
 import OrdersList from '../components/Order/OrdersList';
 import MyButton from '../components/MyButton';
@@ -8,54 +8,54 @@ import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../store/store';
 import { addItem, addOrder, clearCart, removeItem } from '../store/cartSlice';
 
-const NewOrderScreen = ({navigation}) => {
- const dispatch =  useDispatch()
+const NewOrderScreen = ({ navigation }) => {
+  const dispatch = useDispatch()
+  const { items } = useSelector((state: RootState) => state.cart)
+  const totalSum = items.reduce((acc, item) => acc + item.sum, 0);
 
-const handleItemAdd = (item) => {
-  dispatch(addItem(item))
-}
-const handleItemRemove = (item) => {
-  dispatch(removeItem(item))
-}
-const {items} = useSelector((state: RootState) => state.cart)
-const totalSum = items.reduce((acc, item) => acc + item.sum, 0);
-
-const handleOrderPress = useCallback(() => {
-  if (items.length < 1) {
-    Alert.alert('Empty Order', 'Please Add Some Dishes', [{text: 'OK'}], {
-      cancelable: false,
-    });
-  } else {
-    dispatch(addOrder({items: items, sum: totalSum}));
-    navigation.navigate('OrdersScreen');
+  const handleItemAdd = (item) => {
+    dispatch(addItem(item))
   }
-}, []);
+  const handleItemRemove = (item) => {
+    dispatch(removeItem(item))
+  }
+
+  const handleOrderPress = useCallback(() => {
+    if (items.length < 1) {
+      Alert.alert('Empty Order', 'Please Add Some Dishes', [{ text: 'OK' }], {
+        cancelable: false,
+      });
+    } else {
+      dispatch(addOrder({ items: items, sum: totalSum }));
+      navigation.navigate('OrdersScreen');
+    }
+  }, []);
 
   return (
     <View style={styles.screen}>
       <SafeAreaView>
 
-      <OrdersList
-        onAdd={(item) => handleItemAdd(item)}
-        onRemove={(item) => handleItemRemove(item)}
-        deletable
-        addable
-        style={{marginBottom: 107}}
-      />
-      <View style={styles.orderSummary}>
-        <MyText bold style={styles.total}>
-          Total Price:{'   '}
-          <MyText style={styles.totalNumber}>
-            {/* calc the total price */}
-            {totalSum?.toFixed(2)} $
-          </MyText>
-        </MyText>
-        <MyButton
-          title="Order Now"
-          style={{width: 103}}
-          onPress={handleOrderPress}
+        <OrdersList
+          onAdd={(item) => handleItemAdd(item)}
+          onRemove={(item) => handleItemRemove(item)}
+          deletable
+          addable
+          style={{ marginBottom: 107 }}
         />
-      </View>
+        <View style={styles.orderSummary}>
+          <MyText bold style={styles.total}>
+            Total Price:{'   '}
+            <MyText style={styles.totalNumber}>
+              {/* calc the total price */}
+              {totalSum?.toFixed(2)} $
+            </MyText>
+          </MyText>
+          <MyButton
+            title="Order Now"
+            style={{ width: 103 }}
+            onPress={handleOrderPress}
+          />
+        </View>
       </SafeAreaView>
 
     </View>
@@ -74,7 +74,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     shadowColor: 'red',
     shadowOpacity: 0.26,
-    shadowOffset: {width: 0, height: 20},
+    shadowOffset: { width: 0, height: 20 },
     shadowRadius: 8,
     elevation: 5,
     borderRadius: 10,
