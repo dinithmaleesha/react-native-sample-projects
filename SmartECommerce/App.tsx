da5f369c3e4acf08c6e42fc85011e019cbf52905
Dinith Maleesha <maleesha.live123@gmail.com>
2024-09-24 17:47:30 +0530

    custom fonts

--- SmartECommerce/App.tsx
+++ SmartECommerce/App.tsx
@@ -1,13 +1,27 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet  } from 'react-native'
 import React from 'react'
 import { s, vs, ms, mvs } from 'react-native-size-matters';
+import { Fonts } from './src/styles/fonts';
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  customText: {
+    color: 'black',
+    fontSize: 30,
+    fontFamily: Fonts.NunitoLight
+  },
+});
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
+    <View style={styles.main}>
+      <Text style={styles.customText}>Hello, Custom Font!</Text>
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
