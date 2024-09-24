be50c64123d1f6687fcfbc9a11a25a31de6c1a56
Dinith Maleesha <maleesha.live123@gmail.com>
2024-09-24 22:52:26 +0530

    hooks

--- SmartECommerce/App.tsx
+++ SmartECommerce/App.tsx
@@ -1,7 +1,61 @@
-import { View, Text, StyleSheet  } from 'react-native'
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
 import React from 'react'
 import { s, vs, ms, mvs } from 'react-native-size-matters';
 import { Fonts } from './src/styles/fonts';
+import { useForm, Controller } from 'react-hook-form'
+
+const App = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: {
+      errors
+    }
+  } = useForm()
+
+  const submit = (data) => {
+    console.log(data);
+
+  }
+  return (
+    <View style={styles.main}>
+      <Text style={styles.customText}>React Hook Form Example in React Native</Text>
+
+      <Controller
+        name="username"
+        control={control}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            placeholder="Enter Your Name"
+            style={styles.input}
+            value={value}
+            onBlur={onBlur}
+            onChangeText={onChange}
+          />
+        )}
+        rules={{required: true, minLength: 5}}
+      />
+      {errors.username && <Text style={styles.error}>Name is required</Text>}
+
+      <Controller
+        name="email"
+        control={control}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            placeholder="Enter Your Email"
+            style={styles.input}
+            value={value}
+            onBlur={onBlur}
+            onChangeText={onChange}
+          />
+        )}
+      />
+      <Button title='Submit' onPress={handleSubmit(submit)} />
+    </View>
+  );
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   main: {
@@ -11,17 +65,17 @@ const styles = StyleSheet.create({
   },
   customText: {
     color: 'black',
-    fontSize: 30,
     fontFamily: Fonts.NunitoLight
   },
+  input: {
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    width: "90%",
+    marginTop: 18,
+    borderColor: 'gray'
+  },
+  error: {
+    color: 'red'
+  }
 });
-
-const App = () => {
-  return (
-    <View style={styles.main}>
-      <Text style={styles.customText}>Hello, Custom Font!</Text>
-    </View>
-  );
-};
-
-export default App;
